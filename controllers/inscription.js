56b4800f327df9eaaa8ec64f15248392ba35ff54
hugi <hugo.cossec@gmail.com>
2024-11-28 12:14:27 +0100

    feur

--- controllers/inscription.js
+++ controllers/inscription.js
@@ -3,46 +3,40 @@ const router = express.Router();
 const md5 = require("md5");
 const database = require("../models/database.js");
 
-router.get("/", (req, res) => {
+router.get("/", function (req, res) {
   res.render("inscription");
 });
 
-
-router.post("/", (req, res) => {
-  const { login, password, nom, prenom, ddn, email } = req.body;
-
-  if (!login || !password || !nom || !prenom || !ddn || !email) {
-    return res.status(400).send("Tous les champs doivent être remplis !");
-  }
+router.post("/", function (req, res) {
+  const login = req.body.login;
+  const password = req.body.password;
+  const nom = req.body.nom;
+  const prenom = req.body.prenom;
+  const ddn = req.body.ddn;
+  const email = req.body.email;
 
   const hashPass = md5(password);
 
-  const sql = `
-    INSERT INTO utilisateurs (login, password, nom, prenom, ddn, email, type_utilisateur)
-    VALUES (?, ?, ?, ?, ?, ?, 'client')
-  `;
+  const sql = "INSERT INTO utilisateurs (login, password, nom, prenom, ddn, email, type_utilisateur) VALUES (?, ?, ?, ?, ?, ?, 'client')";
 
-  database.query(
-    sql,
-    [login, hashPass, nom, prenom, ddn, email],
-    (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send("Erreur lors de l'inscription");
-      }
-
-      req.session.isAuth = true;
-      req.session.user = {
-        id: result.insertId, 
-        login: login,
-        nom: nom,
-        prenom: prenom,
-        role: "client",
-      };
-      
-      res.redirect("/");
+  database.query(sql, [login, hashPass, nom, prenom, ddn, email], function (err, result) {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Erreur lors de l'inscription");
+      return;
     }
-  );
+
+    req.session.isAuth = true;
+    req.session.user = {
+      id: result.insertId,
+      login: login,
+      nom: nom,
+      prenom: prenom,
+      role: "client"
+    };
+
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
